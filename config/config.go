3296e268975810885ab818b0a44b1fecdb674ae9
forxz <fast35890@gmail.com>
2025-09-07 03:39:26 +0300

    revert config

--- config/config.go
+++ config/config.go
@@ -1,35 +1,18 @@
 package config
 
-import (
-	"os"
-
-	"github.com/joho/godotenv"
-)
+import "github.com/kelseyhightower/envconfig"
 
 type Config struct {
-	PostgresDsn     string
-	GooseMigrations string
-	ApiKey          string
-	RedisDsn        string
+	PostgresDsn     string `envconfig:"POSTGRES_DSN" default:"postgresql://postgres:postgres@localhost:5432/weather_db?sslmode=disable"`
+	GooseMigrations string `envconfig:"GOOSE_MIGRATIONS" default:"./migrations"`
+	ApiKey          string `envconfig:"API_KEY" default:""`
+	RedisDsn        string `envconfig:"REDIS_DSN" default:"redis://localhost:6379/0"`
 }
 
 func LoadConfig() (*Config, error) {
-	// Load .env file
-	if err := godotenv.Load(); err != nil {
+	var cfg Config
+	if err := envconfig.Process("", &cfg); err != nil {
 		return nil, err
 	}
-
-	return &Config{
-		PostgresDsn:     getEnv("POSTGRES_DSN", "postgresql://postgres:postgres@localhost:5432/weather_db?sslmode=disable"),
-		GooseMigrations: getEnv("GOOSE_MIGRATIONS", "./migrations"),
-		ApiKey:          getEnv("API_KEY", ""),
-		RedisDsn:        getEnv("REDIS_DSN", "redis://localhost:6379/0"),
-	}, nil
-}
-
-func getEnv(key, defaultValue string) string {
-	if value, exists := os.LookupEnv(key); exists {
-		return value
-	}
-	return defaultValue
+	return &cfg, nil
 }
--- go.mod
+++ go.mod
@@ -6,7 +6,7 @@ require (
 	github.com/brianvoe/gofakeit/v7 v7.3.0
 	github.com/gofiber/fiber/v2 v2.52.8
 	github.com/jackc/pgx/v5 v5.7.5
-	github.com/joho/godotenv v1.5.1
+	github.com/kelseyhightower/envconfig v1.4.0
 	github.com/pressly/goose/v3 v3.24.3
 	github.com/redis/go-redis/v9 v9.11.0
 	github.com/stretchr/testify v1.10.0
